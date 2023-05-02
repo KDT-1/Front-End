12965d02920d0c29688aadff612114749160bbac
Dayeon-Merry <131240376+Dayeon-Merry@users.noreply.github.com>
2023-05-02 18:56:51 +0900

    Add files via upload

---
+++ frames/성민파트/update_edit.js
@@ -0,0 +1,20 @@
+let switchNow = false;    //스위치 상태 기억할 변수
+function alertToggle() {
+    if (switchNow == false) {  //스위치가 꺼져 있으면
+    document.querySelector(".noticeBox").style.display = "block"; // 보여주고
+    switchNow = true; // 스위치는 켠 상태로 바꿈
+    } else {
+    document.querySelector(".noticeBox").style.display = "none";  // 숨겨주고
+    switchNow = false;  //스위차는 끈 상태로 바꿈
+    }
+}
+    
+function toggleMenu(){
+    let sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+}
+
+function toggleMenu(){
+    let sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+}
