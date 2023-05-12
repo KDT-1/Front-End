2513ff8d4c751be9dcdbf9d644cb52594d1ddf7f
SangDal <131240971+SangDal@users.noreply.github.com>
2023-05-12 22:36:29 +0900

    개인정보수정/관련사이트 2개 생성
    개인 정보 수정 
    - 회원탈퇴 버튼 안되던거 수정 
    관련사이트 
    - patrol (아이프레임 이용해서 신문고띄우기)
    - safety (여성 신고 방법과 전화번호 업로드)

--- frames/최종본html_CD/update.js
+++ frames/최종본html_CD/update.js
@@ -1,12 +1,13 @@
 // update_user_edit.html에서 회원탈퇴 버튼 클릭시 창 띄우기 
 let switchNow = false;    //스위치 상태 기억할 변수
-function alertToggle() {
+function delToggle() {
+    console.log("object");
     if (switchNow == false) {  //스위치가 꺼져 있으면
     document.querySelector(".noticeBox").style.display = "block"; // 보여주고
     switchNow = true; // 스위치는 켠 상태로 바꿈
     } else {
     document.querySelector(".noticeBox").style.display = "none";  // 숨겨주고
-    switchNow = false;  //스위치는 끈 상태로 바꿈
+    switchNow = false;  //스위차는 끈 상태로 바꿈
     }
 }
 
@@ -90,7 +91,7 @@ let switchNow2 = false;
 function PWAToggle() {
     if (switchNow2 == false) {  //스위치가 꺼져 있으면
     document.querySelector(".noticeBoxPWA").style.display = "block"; // 보여주고
-    switchNow = true; // 스위치는 켠 상태로 바꿈
+    switchNow2 = true; // 스위치는 켠 상태로 바꿈
     } else {
     document.querySelector(".noticeBoxPWA").style.display = "none";  // 숨겨주고
     switchNow2 = false;  //스위차는 끈 상태로 바꿈
